6de1b4d5183769db7ed1f0a9be4237ae1a00b2df
Daniel Iglesias <dev.daif@gmail.com>
2023-06-07 11:05:09 -0400

    feat: added commands categories

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import discord
 from discord import app_commands
-
 from decouple import config
+
+import cmds.others as others
+
 
 DISCORD_TOKEN = config('DISCORD_TOKEN')
 
 class Bot(discord.Client):
   async def on_ready(self):
-    await tree.sync()
+    await tree.sync();
     print(f'Logged on as {self.user}!')
   
   async def on_message(self, message):
@@ -19,6 +21,8 @@ intents.message_content = True
 
 client = Bot(intents=intents)
 tree = app_commands.CommandTree(client)
+
+tree.add_command(others.Others())
 
 
 client.run(DISCORD_TOKEN)
---
+++ cmds/others.py
@@ -0,0 +1,24 @@
+import discord
+from discord import app_commands
+
+print("Hello commands")
+
+class Others(app_commands.Group):
+        @app_commands.command(name="ping", description="Ping!")
+        async def ping(self, interaction: discord.Interaction):
+                print(interaction.user.mention)
+                await interaction.response.send_message(f"{interaction.user.mention}, pong !")
+
+        @app_commands.command(name="melenas", description="Who is the MELENAS?")
+        async def melenas(self, interaction: discord.Interaction):
+                await interaction.response.send_message(f"<@621975714287321094>, MELENAS !")
+
+        @app_commands.command(name="sudaka", description="Who is the SUDAKA?")
+        async def sudaka(self, interaction: discord.Interaction):
+                await interaction.response.send_message(
+            f"<@632153965835976704> y <@317876695652630536>, SUDAKAS!"
+        )
+
+        @app_commands.command(name="putero", description="Who is the PUTERO?")
+        async def putero(self, interaction: discord.Interaction):
+                await interaction.response.send_message(f"<@621964049789222951>, putero!")
