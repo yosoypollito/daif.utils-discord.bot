ef24d5a63a53ccfcaee97fce7ac5b8741b4575f6
Daniel Iglesias <dev.daif@gmail.com>
2023-06-08 03:03:08 -0400

    feat: added translate api.

--- services/translate.py
+++ services/translate.py
@@ -1,3 +1,21 @@
+import requests
+from decouple import config
+
+url = config("MICROSOFT_TRANSLATE_URL")
+token = config("MICROSOFT_TRANSLATE_API_KEY")
+
 async def translate(from_:str, to:str, text:str):
-    print(f"{from_}, {to}, {text}")
-    return text
+    
+    headers = {
+        "Content-Type":"application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    
+    response = requests.get(f"{url}?from={from_}&to={to}&text={text}", headers=headers)
+
+    response_json = response.json()
+    
+    translated = response_json["translated"]
+    time_spend = response_json["time"]
+    
+    return f"{translated} \n `{time_spend} ms`"
