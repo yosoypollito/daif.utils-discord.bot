9ab393bbed35c9b22de08543206a9fe70aa7ba69
Daniel Iglesias <dev.daif@gmail.com>
2023-06-08 05:49:52 -0400

    feat: added describe to params

--- cmds/translate.py
+++ cmds/translate.py
@@ -133,7 +133,7 @@ print("loading translate commands")
 
 class Translate(commands.Cog):
     @app_commands.command(name="translate", description="Translate text using Microsoft Translation!")
-    @app_commands.describe(text="The text you want to translate")
+    @app_commands.describe(text="The text you want to translate", lang="The language you want to translate from", to="The language you want to translate to")
     @app_commands.rename(text="text")
     async def translate(self, interaction: Interaction, lang: str, to:str, text:str):
         translatedText = await services.translate(lang, to, text) 
