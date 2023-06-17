5ab6db7aa7a63af141c6734d321d479c2fe2aac9
Daniel Iglesias <dev.daif@gmail.com>
2023-06-17 10:32:49 -0400

    feat: added reset ip to dall_e

--- services/open_ai/dall_e.py
+++ services/open_ai/dall_e.py
@@ -1,20 +1,29 @@
 import openai
 import urllib.request as requests
 
+from .proxy_utils import reset_ip
+
 from .config import openAIClient 
 from log import Log, Feature
+
+from utils import fake_useragent
 
 log = Log("dall_e", Feature.Command)
 
 async def generateImage(prompt):
   log.info("Generating image")
-  response = openAIClient.Image.create(prompt=prompt, n=1, size="1024x1024")
+  try:
+    response = openAIClient.Image.create(prompt=prompt, n=1, size="1024x1024")
+  except Exception as e:
+    print("error", e.args)
+    if "authorized ip address" in e.args[0].lower():
+      await reset_ip()
+      return await generateImage(prompt)
   
   log.info("Image created")
   
   img_url = response["data"][0]["url"]
   
-  fake_useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
   r = requests.Request(img_url, headers={'User-Agent': fake_useragent})
   f = requests.urlopen(r)
   
