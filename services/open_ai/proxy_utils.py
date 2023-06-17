5140ce86f13f5603f543924d2328639c8cba8344
Daniel Iglesias <dev.daif@gmail.com>
2023-06-17 10:33:23 -0400

    feat: added user agent to proxy ip reset

--- services/open_ai/proxy_utils.py
+++ services/open_ai/proxy_utils.py
@@ -2,17 +2,26 @@ import requests
 
 from decouple import config
 
+from utils import fake_useragent
+
 api_key = config("OPEN_AI_API_KEY")
 proxy_host = config("OPEN_AI_PROXY_HOST")
 async def reset_ip():
     print("Reseting proxy ip address...")
     
+    print(api_key, proxy_host)
+    
     headers = {
       "Authorization": f"Bearer {api_key}",
       "Content-Type": "application/json",
+      "User-Agent": fake_useragent
     }
-
-    response = requests.post(f"{proxy_host}/resetip", headers=headers)
+    
+    try:
+        response = requests.post(f"{proxy_host}/resetip", headers=headers)
+    except Exception as e:
+        print("Error resetting proxy ip address")
+        return False
     
     print("Done!")
     
