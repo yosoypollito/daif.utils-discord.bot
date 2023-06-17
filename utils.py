187f9f7f28ae44ed6043cfc0d0ab8f3391ca7800
Daniel Iglesias <dev.daif@gmail.com>
2023-06-17 10:32:58 -0400

    feat: added fake_useragent

--- utils.py
+++ utils.py
@@ -9,3 +9,5 @@ def get_auto_complete(text: str, list: dict[any]):
         if key.lower().startswith(text.lower()) or value.lower().startswith(text.lower()):
             data.append(Choice(name=f"{value} ({key})", value=key))
     return data[0:24]
+
+fake_useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
